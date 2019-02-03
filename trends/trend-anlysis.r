108966e89d07ff4ecd936f9276cdcafe1ec69a56
peterdalle <peterdalle@gmail.com>
2019-02-03 01:04:39 +0100

    Compare trends, analysis

--- trends/trend-anlysis.r
+++ trends/trend-anlysis.r
@@ -1,21 +1,41 @@
 # Simple analyses of trends.
-
 library(tidyverse)
+library(lubridate)
+library(gridExtra)
 
-# Read CSV file with 2 headers: datetime,trend
-df1 <- read.csv("2014-12-19_to_2016-03-29_trends_sweden.csv",
-                header=TRUE, sep=",", encoding="UTF-8")
+# Function to read trend files.
+read_trends <- function(filename) {
+  # Read .zip or .csv?
+  if (endsWith(tolower(filename), ".zip")) {
+    df <- read.csv(unz(filename, sub(".zip", "", filename)), 
+                   sep=",", encoding="UTF-8")
+  } else {
+    df <- read.csv(filename, 
+                   sep=",", encoding="UTF-8")
+  }
+  # Old or new format? Old format has 2 headers.
+  # Add missing headers to match new format.
+  if (ncol(df) == 2) {
+    df$tweetvolume <- NA
+    df$promotedcontent <- NA
+  }
+  # Set as date.
+  df$datetime <- lubridate::ymd_hm(df$datetime)
+  return(df)
+}
 
-# Read CSV file with 4 headers: datetime,trend,tweetvolume,promotedcontent
-df2 <- read.csv("2016-03-29_to_2018-07-20_trends_sweden.csv",
-                header=TRUE, sep=",", encoding="UTF-8")
+# Read trends from files.
+df1 <- read_trends("2014-12-18_to_2016-03-29_trends_sweden.csv.zip")
+df2 <- read_trends("2016-03-29_to_2018-07-20_trends_sweden.csv.zip")
+df3 <- read_trends("trends_sweden.csv.zip")
 
-df3 <- read.csv("trends_sweden.csv", 
-                header=TRUE, sep=",", encoding="UTF-8")
+#df1 <- read_trends("2014-12-18_to_2016-03-29_trends_sthlm.csv.zip")
+#df2 <- read_trends("2016-03-29_to_2018-07-20_trends_sthlm.csv.zip")
+#df3 <- read_trends("trends_sthlm.csv.zip")
 
-# Add missing headers to CSV data with 2 headers.
-df1$tweetvolume <- NA
-df1$promotedcontent <- NA
+#df1 <- read_trends("2014-12-19_to_2016-03-29_trends_gbg.csv.zip")
+#df2 <- read_trends("2016-03-29_to_2018-07-20_trends_gbg.csv.zip")
+#df3 <- read_trends("trends_gbg.csv.zip")
 
 # Combine all rows into a single data frame.
 trends <- rbind(df1, df2, df3)
@@ -25,9 +45,6 @@ rm(df1, df2, df3)
 
 # How many trends do we have?
 nrow(trends)
-
-# Set data type: date (This is slow. Use lubridate if too slow.)
-#trends$datetime <- as.Date(trends$datetime)
 
 # Plot most trended.
 trends %>% 
@@ -42,3 +59,43 @@ trends %>%
     labs(title = "Mest trendade hashtaggarna i Sverige 2014 till 2019",
          caption = paste0("Baserat på ", nrow(trends), " trender"),
          x = NULL)
+
+# Plot number of times a specific trend has trended by month.
+trends %>% 
+  mutate(year_month = lubridate::floor_date(datetime, unit="month"),
+         week = lubridate::week(datetime),
+         year = lubridate::year(datetime)) %>% 
+  group_by(trend, year_month) %>% 
+  count(sort=TRUE) %>% 
+  ungroup() %>% 
+  filter(trend %in% c("#svpol", "#svmed", "#lyrikfredag", "#ifkgbg")) %>%
+  ggplot(aes(year_month, n, fill=n)) +
+    geom_col() +
+    theme_bw() +
+    theme(legend.position = "none") +
+    labs(title = "Antal gånger som trend har trendat per månad i Sverige 2014 till 2019",
+         caption = paste0("Baserat på ", nrow(trends), " trender"),
+         x = NULL,
+         y = NULL) +
+    facet_wrap(~ trend, ncol=2)
+
+# Plot relative number of times a specific trend has trended by month.
+trends %>% 
+  mutate(year_month = lubridate::floor_date(datetime, unit="month"),
+         week = lubridate::week(datetime),
+         year = lubridate::year(datetime)) %>% 
+  group_by(year_month, trend) %>% 
+  summarize(n = n()) %>%
+  mutate(percent = n / sum(n) * 100) %>%
+  ungroup() %>% 
+  filter(trend %in% c("#svpol", "#svmed", "#lyrikfredag", "#ifkgbg")) %>%
+  ggplot(aes(year_month, percent, fill=n)) +
+    geom_col() +
+    theme_bw() +
+    theme(legend.position = "none") +
+    labs(title = "Relativ frekvens av trend per månad i Sverige 2014 till 2019",
+         subtitle = "Relativt till alla trender per månad",
+         caption = paste0("Baserat på ", nrow(trends), " trender"),
+         x = NULL,
+         y = "Procent") +
+    facet_wrap(~ trend)
